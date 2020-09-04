4b3552363a74ad21ec1289146e57641b63d1db28
Tommi Aalto <tommi.j.aalto@tuni.fi>
2020-09-05 01:53:34 +0300

    update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,34 @@
 <template>
-  <div class="hello">
-    <p>Welcome to Tommi Aaltos homepage. This page is running on AWS.
-       Testing deploying and configuring. Updates coming soon. :)</p>
-  </div>
+    <div class="main">
+    <header>
+        <h3>Tommi Aalto</h3>
+        <nav>
+            <a href="/html/">HTML</a> |
+            <a href="/css/">CSS</a> |
+            <a href="/js/">JavaScript</a> |
+            <a href="/python/">Python</a>
+        </nav>
+    </header>
+    <div class="card">
+        <img class="profile-pic" src="../assets/profile-pic2.jpg" alt="Tommi">
+        <p>Tommi Aalto</p>
+        <p>Software Engineer</p>
+    </div>
+    <div class="content">
+    <h1>Hello!</h1>
+    <p>I've created online CV for you!</p>
+    <p>You can find all professional information about me and my skills on this page.</p>
+    <p>For other intrest check my political and social blog and my social media from here:</p>
+    
+    <div>
+
+    </div>
+
+    <p>Sincerely, <br>
+      <span style="margin-left: 20px;"><em id="signature">Tommi Aalto</em></span>
+    </p>
+    </div>
+    </div>
 </template>
 
 <script>
@@ -16,18 +42,27 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+template {
+        
+        background-image: url("../assets/background_tracks.jpg");
+      }
+      .main {
+        background-color: linen;
+        margin: 24px 48px 24px 48px;
+        border: 1px solid black;
+        border-radius: 35px;
+      }
+      .card {
+        margin: auto;
+        width: 40%;
+        border: 1px solid black;
+      }
+      .profile-pic {
+        margin: 24px 48px 24px 48px;
+        width: 40%;
+        height: 40%;
+        border-radius: 50%;
+        border: 2px solid black;
+        text-align: center;
+      } 
 </style>
